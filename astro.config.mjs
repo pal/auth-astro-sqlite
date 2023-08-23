713c90d55a01ab251616b39115e18bb227b5636c
Pål <pal@subtree.se>
2023-08-23 20:10:50 +0200

    npx astro add node

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import auth from "auth-astro";
+import auth from 'auth-astro';
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), auth()]
+  integrations: [mdx(), sitemap(), auth()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
